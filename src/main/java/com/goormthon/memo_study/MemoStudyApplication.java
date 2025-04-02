019b168aecdda4a923076ad146f710308add271c
gihhyeon <fineblue.kh@gmail.com>
2025-04-02 20:11:38 +0900

    init project

---
+++ src/main/java/com/goormthon/memo_study/MemoStudyApplication.java
@@ -0,0 +1,13 @@
+package com.goormthon.memo_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemoStudyApplication.class, args);
+	}
+
+}
